676647262872f1e0dd77c590400647419465795f
hyowon3434 <hyowon3434@naver.com>
2024-05-30 22:20:18 +0900

    Feat: added join process

---
+++ security1/src/main/java/com/example/security1/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터(SecurityConfig class)가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .requestMatchers("/user/**").authenticated()
+                .requestMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm");
+
+                return http.build();
+    }
+
+    @Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    public BCryptPasswordEncoder bCryptPasswordEncoder() throws Exception{
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ security1/src/main/java/com/example/security1/controller/IndexController.java
@@ -0,0 +1,65 @@
+package com.example.security1.controller;
+
+import com.example.security1.model.User;
+import com.example.security1.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
+    public String index(){
+        // 머스테치 기본폴터 src/main/resources/
+        // 뷰리졸버 설정: templates(prefix), .mustache(suffix) // 색량가능!!
+        return "index"; // src/main.resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public @ResponseBody String join(User user){
+        String rawPW = user.getPassword();
+        String encPW = bCryptPasswordEncoder.encode(rawPW);
+        user.setPassword(encPW);
+        user.setRole("ROLE_USER");
+
+        userRepository.save(user); // 회원가입 잘됨. 비밀번호: 1234 => 시큐리티로 로그인 x. 패스워드가 암호화 안됨.
+        log.warn(user.toString());
+        return "redirect:/loginForm";
+    }
+}
